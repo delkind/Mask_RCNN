b9cb8c3e4239f1cceed8c76a3aceb4a13724bc08
David Elkind <david.elkind@gmail.com>
2019-10-28 21:27:15 +0200

    Fixed inference for full image

--- samples/hippocampus/predict_full.py
+++ samples/hippocampus/predict_full.py
@@ -36,7 +36,7 @@ def predict_full_image(weights, image_path, crop_size, border_size, backbone='re
     crops, image = split_image(image_path, crop_size, border_size)
     model = create_model(backbone, weights)
 
-    results = [model.detect([crop[0]], verbose=0) for crop in crops]
+    results = [model.detect([crop[0]], verbose=0)[0] for crop in crops]
     results = [adjust_results(border_size, coords, crop_size, image, result)
                for (crop, coords), result in zip(crops, results)]
     final_results = {k: np.concatenate([res[k] for res in results], axis=0)
